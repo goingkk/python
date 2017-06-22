f1b8a164a569047fabc0b28b2045d99512c17a99
goingkk <453507217@qq.com>
2017-06-22 16:25:06 +0800

    add super.py

---
+++ super.py
@@ -0,0 +1,49 @@
+#coding:utf-8
+
+class A():
+	def __init__(self):
+		print("in A")
+		print("out A")
+	
+
+	
+#######
+class B(A):
+	def __init__(self):
+		print("in B")
+		super(B,self).__init__()
+		print("out B")
+	
+class C(A):
+	def __init__(self):
+		print("in C")
+		super(C,self).__init__()
+		print("out C")
+	
+		
+#######
+class D(C,B):
+	def __init__(self):
+		print("in D")
+		print("-------------------")
+		super(B,self).__init__()      #     A A
+		print("-------------------")
+		super(C,self).__init__()      #   B A A B
+		print("-------------------")
+		super(D,self).__init__()      # C B A A B C
+		print("-------------------")
+		print("out D")
+	
+		
+#######
+D()
+print(D.__mro__)
+
+"""
+	D 的 mro 执行顺序是 D > C > B > A > object，
+	super(X,self) 是指在当前类的mro顺序里面，X的下一个，并不是指父类，
+	所以，super(B,self).__init__() == A.__init__()
+		  super(C,self).__init__() == B.__init__() > A.__init__()
+		  super(D,self).__init__() == C.__init__() > B.__init__() > A.__init__()
+"""
+	
